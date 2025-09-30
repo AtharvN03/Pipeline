451c76c92d067cfdb45ba97562ff337c44375ae6
Atharv <naraleatharv07@gmail.com>
2025-09-30 17:06:26 +0530

    Add HTML,CSS,and JS files

---
+++ dicee.js.js
@@ -0,0 +1,24 @@
+var randomNumber = Math.floor(Math.random()*6)+1;
+
+var randomdiceImage = "dice"+randomNumber+".png";
+
+var randomImageSource = "images/"+randomdiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomdiceImage2 = "dice"+randomNumber2+".png"
+var randomImageSource2 = "images/"+randomdiceImage2;
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+
+if(randomNumber>randomNumber2){
+    document.querySelector("h2").innerHTML="Player 1 Wins !!!";
+}
+else if(randomNumber2>randomNumber){
+    document.querySelector("h2").innerHTML="Player 2 Wins !!!";
+}
+else{
+    document.querySelector("h").innerHTML="Tie !!!";
+}
